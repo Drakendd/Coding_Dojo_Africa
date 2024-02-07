49eede3ff732d8ae6ad8b0840f23c97a5ad03bf0
Drakendd <yassin.daira@gmail.com>
2024-02-07 11:36:55 +0100

    video-preview

---
+++ Web Fundamentals/week_2/Day_2/Practice/video-preview/script.js
@@ -0,0 +1,9 @@
+
+console.log("page loaded...");
+function play(){
+    var x=document.getElementById("vd");
+    x.play();
+}function pause(){
+    var x=document.getElementById("vd");
+    x.pause();
+}
