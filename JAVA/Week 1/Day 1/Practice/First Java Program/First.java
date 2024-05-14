928ca100f4bbf3d8347686ffa6ddaa7df4e83c9b
Drakendd <yassin.daira@gmail.com>
2024-05-14 13:46:29 +0100

    First java program

---
+++ JAVA/Week 1/Day 1/Practice/First Java Program/First.java
@@ -0,0 +1,10 @@
+public class First{
+    public static void main(String[]args){
+        String name = "Yassin";
+        int age = 21;
+        String town = "kef";
+    System.out.println("my name is " +name);
+    System.out.println("im" +age+ "years old");
+    System.out.println("my home town is"+town);
+    }
+}
